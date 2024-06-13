a450c8be59f8fd8281e83369b9f14eba282b6e4c
Administrator <zakharchuksasha7@gmail.com>
2024-06-13 13:57:16 +0300

    added modal window

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,120 +1,139 @@
 import React, {useEffect, useState} from "react";
-import { SimpleModal } from "../SimpleModal/SimpleModal";
+import {SimpleModal} from "../SimpleModal/SimpleModal";
 import "./Main.css";
 
 export const Main = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   const [modalInfoIsOpen, setModalInfoOpen] = useState(false);
-  const [modalAboutIsOpen, setModalAboutOpen] = useState(false);
-  const [modalDataIsOpen, setModalDataOpen] = useState(false);
+
+  const openModal = () => {
+    setModalInfoOpen(true);
+  };
 
   return (
-    <main className="main">
-      <section className="section">
-        <h2>Зформуй заявку на доставку води</h2>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <img className="thumbnail" id="avatar" src=""/>
-              </li>
-              <li className="nav-item active">
-                <a className="nav-link" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <button
-                    className="modal-show-button"
-                    onClick={() => setModalInfoOpen(true)}
-                >
-                  Add
-                </button>
-                <a className="nav-link" href="#" data-toggle="modal" data-target="#addModal"></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#" id="deleteButton">Delete</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/logout">Logout</a>
-              </li>
-            </ul>
-            <span className="navbar-text">
-                <div id="login">...</div>
-            </span>
+      <div className="col-auto">
+        <nav className="navbar">
+          <div className="navbar-nav">
+            <div className="nav-item">
+              <img className="thumbnail" id="avatar" src=""/>
+            </div>
+            <div className="nav-item">
+              <a className="nav-link" href="/">Home</a>
+            </div>
+            <div className="nav-item">
+              <a className="nav-link" href="#" onClick={openModal}>Add</a>
+            </div>
+            <div className="nav-item">
+              <a className="nav-link" href="#" id="deleteButton">Delete</a>
+            </div>
+            <div className="nav-item">
+              <a className="nav-link" href="/logout">Logout</a>
+            </div>
           </div>
+          <span className="navbar-text">
+    <div id="login">...</div>
+  </span>
         </nav>
-        <button
-            className="modal-show-button"
-            onClick={() => setModalInfoOpen(true)}
-        >
-          Modal Info
-        </button>
-        <button
-            className="modal-show-button"
-            onClick={() => setModalAboutOpen(true)}
-        >
-          Modal About
-        </button>
-        <button
-            className="modal-show-button"
-            onClick={() => setModalDataOpen(true)}
-        >
-          Modal Data
-        </button>
+
+
+        <div className="col">
+          <table id="data" className="table table-striped">
+            <thead>
+            <tr>
+              <th scope="col">Check</th>
+              <th scope="col">Date</th>
+              <th scope="col">Address</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Price</th>
+              <th scope="col">TaskOwner</th>
+              <th scope="col">TaskStatus</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+          </table>
+          <nav aria-label="Page navigation">
+            <ul id="pages" className="pagination"></ul>
+          </nav>
+        </div>
 
         <SimpleModal
             isOpen={modalInfoIsOpen}
             onClose={() => setModalInfoOpen(false)}
         >
           <h2>Modal Info</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo,
-            dolorum dolore vero eligendi cum iusto ea quam perspiciatis
-            veritatis rem cupiditate a in, aliquam pariatur.
-          </p>
+          <div id="addModal" className="modal fade" role="dialog">
+            <div className="modal-dialog modal-dialog-centered modal-lg">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h4 className="modal-title">Enter new task</h4>
+                </div>
+                <div className="modal-body">
+                  <table>
+                    <tbody>
+                    <tr>
+                      <td>
+                        Delivery Date: <input type="datetime-local" id="deliveryDate"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Address: <input type="area" id="address" placeholder="address"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Phone number: <input type="text" id="phone" placeholder="phone"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Quantity: <input type="text" id="quantity" onChange="calculatePrice()" placeholder="quantity"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Price: <input type="text" id="price" disabled></input>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><span id="dateSpan"></span></td>
+                    </tr>
+                    <tr>
+                      <td><span id="addressSpan"></span></td>
+                    </tr>
+                    <tr>
+                      <td><span id="quantitySpan"></span></td>
+                    </tr>
+                    <tr>
+                      <td><span id="phoneSpan"></span></td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input type="button" id="submitButton" value="Add"/>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><span id="messageSpan"></span></td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-default" onClick={() => setModalInfoOpen(false)}>Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </SimpleModal>
-
-        <SimpleModal
-            isOpen={modalAboutIsOpen}
-            onClose={() => setModalAboutOpen(false)}
-        >
-          <h2>Modal About</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus
-            placeat commodi distinctio sunt nesciunt quia provident repudiandae
-            totam laudantium vel maiores rerum cupiditate, hic repellat error
-            odio itaque ratione blanditiis? Dolorum excepturi quibusdam repellat
-            illum, recusandae consectetur veniam velit minima, totam assumenda
-            dignissimos iure illo obcaecati consequatur maiores. Molestiae, ut.
-          </p>
-        </SimpleModal>
-
-        <SimpleModal
-            isOpen={modalDataIsOpen}
-            onClose={() => setModalDataOpen(false)}
-        >
-          <h2>Modal Data</h2>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            nemo quae, dolorem molestias aliquam aut quia minima eos quis
-            sapiente unde veniam aliquid sequi saepe iste exercitationem,
-            nesciunt enim doloremque. Veritatis, quaerat! Beatae corrupti velit
-            officia aspernatur impedit nam mollitia laborum itaque totam
-            explicabo obcaecati, quidem dignissimos consectetur hic soluta, in
-            eaque enim possimus fuga? Numquam sint hic aut debitis nobis.
-            Doloremque magni laborum ab quidem velit maxime, neque quia.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio non,
-            incidunt neque delectus asperiores nam! Tenetur reprehenderit hic
-            eum quasi quia impedit veritatis laudantium facere optio odit
-            pariatur, quo corrupti ratione? Consectetur numquam aliquid cumque
-            ipsam repellat id ducimus, optio blanditiis maiores fuga eligendi
-            quia.
-          </p>
-        </SimpleModal>
-      </section>
-    </main>
+      </div>
   );
 };
+
+export default {Main};
